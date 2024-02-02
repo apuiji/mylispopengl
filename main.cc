36777b1f5009fdf669cc166266c558d11baf53f2
apuiji <2330683684@qq.com>
2024-02-02 18:14:48 +0800

    a

--- main.cc
+++ main.cc
@@ -1,12 +1,39 @@
-#include<SDL2/SDL.h>
-#include"mylisp/value.hh"
+#include<GL/glew.h>
+#include<GLFW/glfw3.h>
+#include<GL/gl.h>
+#include<glm/glm.hpp>
+#include"mylisp/object.hh"
 
-inline Value load1();
-
-extern "c" {
-  Value load() {
-    return load1();
+namespace zlt::mylisp::opengl {
+  static inline Value exp0rt() {
+    ;
   }
 }
 
-inline Value load1() {}
+extern "c" {
+  Value mylispExport() {
+    return zlt::mylisp::opengl::exp0rt();
+  }
+}
+
+namespace zlt::mylisp::opengl {
+  static NativeFunction openWindow;
+  static NativeFunction closeWindow;
+
+  inline Value exp0rt() {
+    MapObj::StrPool sp;
+    sp[constring<'o', 'p', 'e', 'n', 'W', 'i', 'n', 'd', 'o', 'w'>] = openWindow;
+    sp[constring<'c', 'l', 'o', 's', 'e', 'W', 'i', 'n', 'd', 'o', 'w'>] = closeWindow;
+    MapObj *mod = neobj<MapObj>();
+    return mod;
+  }
+
+  Value openWindow(const Value *it, const Value *end) {
+    double width;
+    double height;
+    wstring_view title;
+    if (!dynamicast(make_tuple(&width, &height, &title), it, end)) {
+      return Null();
+    }
+  }
+}
