12c50b8755e8f78a7fcf4587fd40291cb05ea0d2
apuiji <2330683684@qq.com>
2024-02-04 16:08:49 +0800

    a

--- main.cc
+++ main.cc
@@ -2,7 +2,9 @@
 #include<GLFW/glfw3.h>
 #include<GL/gl.h>
 #include<glm/glm.hpp>
-#include"mylisp/object.hh"
+#include"mylisp/iconvs.hh"
+
+using namespace std;
 
 namespace zlt::mylisp::opengl {
   static inline Value exp0rt() {
@@ -17,6 +19,11 @@ extern "c" {
 }
 
 namespace zlt::mylisp::opengl {
+  struct WindowObj final: Object {
+    GLFWwindow *value;
+    WindowObj(GLFwindow *value) noexcept: value(value) {}
+  };
+
   static NativeFunction openWindow;
   static NativeFunction closeWindow;
 
@@ -25,15 +32,25 @@ namespace zlt::mylisp::opengl {
     sp[constring<'o', 'p', 'e', 'n', 'W', 'i', 'n', 'd', 'o', 'w'>] = openWindow;
     sp[constring<'c', 'l', 'o', 's', 'e', 'W', 'i', 'n', 'd', 'o', 'w'>] = closeWindow;
     MapObj *mod = neobj<MapObj>();
+    mod->strPool = std::move(sp);
     return mod;
   }
 
   Value openWindow(const Value *it, const Value *end) {
-    double width;
-    double height;
+    int width;
+    int height;
     wstring_view title;
-    if (!dynamicasts(make_tuple(&width, &height, &title), it, end)) {
+    if (!dynamicasts(make_tuple(&width, &height, &title), it, end)) [[unlikely]] {
       return Null();
     }
+    string title1;
+    if (!strconv(title1, title)) {
+      title1 = "";
+    }
+    auto window = glfwCreateWindow(width, height, title1.data(), nullptr, nullptr);
+    if (!window) {
+      return Null();
+    }
+    return neobj<WindowObj>(window);
   }
 }
